d0059c18522fc065acf919ba6ab3f11ee338605e
Armin Oonk <armin@berkelaarmrt.com>
2016-12-15 22:37:05 +0100

    Finished day 7

--- day7.py
+++ day7.py
@@ -5,6 +5,7 @@ def contains_abba(l):
 
     return False
 
+
 def is_abba(l):
     return l[:2] == l[:1:-1] and l[0] != l[1]
 
@@ -17,6 +18,33 @@ def is_tls(net_list, hyper_list):
     for nl in net_list:
         if contains_abba(nl):
             return True
+    return False
+
+
+def extract_bab(l):
+    ret = []
+
+    for i in range(0, len(l) - 2):
+        if is_bab(l[i:i + 3]):
+            ret.append(l[i:i + 3])
+
+    return ret
+
+
+def is_bab(l):
+    return l[0] == l[2] and l[0] != l[1]
+
+
+def is_ssl(net_list, hyper_list):
+    bab_list = []
+    for hl in hyper_list:
+        bab_list += extract_bab(hl)
+
+    for nl in net_list:
+        for bab in bab_list:
+            aba = bab[1] + bab[0] + bab[1]
+            if aba in nl:
+                return True
     return False
 
 
@@ -42,11 +70,16 @@ with open('inputDay7.txt', 'r') as f:
     read_data = f.readlines()
 
 number_of_tls = 0
+number_of_ssl = 0
 for l in read_data:
     nl ,hl = extract_nets(l.strip())
 
-    if is_tls(net_list=nl, hyper_list=hl):
-        print(l + " is TLS")
-        number_of_tls += 1
+    if is_ssl(net_list=nl, hyper_list=hl):
+        print(l + ' is SSL')
+        number_of_ssl += 1
+    # if is_tls(net_list=nl, hyper_list=hl):
+    #     print(l + " is TLS")
+    #     number_of_tls += 1
 
 print('Number of TLS: ' + str(number_of_tls))
+print('Number of SSL: ' + str(number_of_ssl))
