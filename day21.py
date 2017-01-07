33a679ce02b949390117d01d683afa57ffe9f6b1
Armin Oonk <armin@berkelaarmrt.com>
2017-01-07 19:15:07 +0100

    Day 21 part 1

--- day21.py
+++ day21.py
@@ -52,39 +52,85 @@ def rotate_on_letter(txt, x):
 
 def move_position(txt, x, y):
     ret = txt[:x] + txt[x+1:]
-    if y == 0:
-        return txt[x] + ret
-    return ret[:y] + txt[x] + ret[y+1:]
+    print('ret: ' + ret)
+    # if y == 0:
+    #     return txt[x] + ret
+    return ret[:y] + txt[x] + ret[y:]
 
 
 with open('inputDay21.txt', 'r') as f:
     data = f.readlines()
 
-password = 'abcde'
+# password = 'abcde'
+password = 'abcdefgh'
 
-password = swap_position(password, 4, 0)
-print('Swap position 4, 0: ' + password)
+# txt = move_position(password, 3, 2)
+# print(txt)
+# quit()
+for d in data:
+    if d.startswith('swap position'):
+        vals = d.split(' ')
+        x = int(vals[2])
+        y = int(vals[5])
+        password = swap_position(password, x, y)
+        print('Swapping position: ' + str(x) + ' ' + str(y) + ' result: ' + password)
+    elif d.startswith('swap letter'):
+        vals = d.split(' ')
+        x = vals[2].strip()
+        y = vals[5].strip()
+        password = swap_letter(password, x, y)
+        print('Swapping letter: ' + x + ' ' + y + ' result: ' + password)
+    elif d.startswith('rotate based on position of letter'):
+        vals = d.split(' ')
+        x = vals[6].strip()
+        password = rotate_on_letter(password, x)
+        print('Rotate based on letter ' + x + ' result: ' + password)
+    elif d.startswith('rotate'):
+        vals = d.split(' ')
+        rot = int(vals[2])
+        if vals[1] == 'left':
+            password = rotate_left(password, rot)
+            print('Rotating left nr steps ' + str(rot) + ' result: ' + password)
+        elif vals[1] == 'right':
+            password = rotate_right(password, rot)
+            print('Rotating right nr steps ' + str(rot) + ' result: ' + password)
+        else:
+            print('Unknown rotation')
+    elif d.startswith('move position'):
+        vals = d.split(' ')
+        x = int(vals[2])
+        y = int(vals[5])
+        password = move_position(password, x, y)
+        print('Moving position ' + str(x) + ' with: ' + str(y) + ' result: ' + password)
+    elif d.startswith('reverse positions'):
+        vals = d.split(' ')
+        x = int(vals[2])
+        y = int(vals[4])
+        password = reverse_position(password, x, y)
+        print('Reversing position ' + str(x) + ' and ' + str(y) + ' result: ' + password)
+    else:
+        print('Unknown command: ' + d)
 
-password = swap_letter(password, 'd', 'b')
-print("Swap letter 'd' 'b': " + password)
-
-password = reverse_position(password, 0, 4)
-print('Reverse position 0 4: ' + password)
-
-password = rotate_left(password, 1)
-print("Rotate left: " + password)
-
-# password = rotate_right(password, 2)
-# print("Rotate right: " + password)
-
-password = move_position(password, 1, 4)
-print('Move position 1 to 4: ' + password)
-
-password = move_position(password, 3, 0)
-print('Move position 3 to 0: ' + password)
-
-password = rotate_on_letter(password, 'b')
-print("Rotate on letter b: " + password)
-
-password = rotate_on_letter(password, 'd')
-print("Rotate on letter d: " + password)
+# password = swap_position(password, 4, 0)
+# print('Swap position 4, 0: ' + password)
+#
+# password = swap_letter(password, 'd', 'b')
+# print("Swap letter 'd' 'b': " + password)
+#
+# password = reverse_position(password, 0, 4)
+# print('Reverse position 0 4: ' + password)
+#
+# password = rotate_left(password, 1)
+# print("Rotate left: " + password)
+#
+# password = move_position(password, 1, 4)
+# print('Move position 1 to 4: ' + password)
+#
+# password = move_position(password, 3, 0)
+# print('Move position 3 to 0: ' + password)
+#
+# password = rotate_on_letter(password, 'b')
+# print("Rotate on letter b: " + password)
+#
+# password = rotate_on_letter(password, 'd')
+# print("Rotate on letter d: " + password)
